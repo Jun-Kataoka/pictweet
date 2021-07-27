dcee4a197d8421d8ec5e5e69cccc21c78495e09b
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-28 00:51:59 +0900

    バリデーションの変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
+  # imageも空で投稿できないように追記
+  validates :text, :image, presence: true
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
 
